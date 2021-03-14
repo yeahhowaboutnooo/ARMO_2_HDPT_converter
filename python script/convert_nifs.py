ae883e212cb4665903c31aab23688636342bc8fa
yeahhowaboutnooo <62910188+yeahhowaboutnooo@users.noreply.github.com>
2021-03-14 11:13:37 +0100

    files

---
+++ python script/convert_nifs.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+
+# ***** BEGIN LICENSE BLOCK *****
+#
+#Copyright (c) 2021, yeahhowaboutnooo.
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+#
+# ***** END LICENSE BLOCK *****
+
+import sys, os, glob, re
+
+from pyffi.formats.nif import NifFormat
+import multiprocessing as mp
+
+# sys.frozen does not exist, unless it's been set by pyinstaller
+# -> if it does not exist getattr returns false
+if not getattr( sys, 'frozen', False ):
+	os.chdir(os.path.dirname(__file__))
+
+#super dirty: windows does not allow \/:*?"<>| in file names
+#-> because ':' is fairly common for meshnames -> convert ':' to ';' and disallow ';' instead
+#(obviously the xEdit delphi script then does the reverse conversion from ';' to ':')
+#and no i don't like this "solution" either :P
+#compile this regex here for later use
+rec = re.compile(r"[\\\/;\*\?<>\|]")
+
+
+files = glob.glob('./**/*.[nN][iI][fF]', recursive=True)
+#remove previously generated _extraParts from the file-list
+extraParts = '__HDPT_extraParts'
+files = [f for f in files if extraParts not in f]
+
+def convertFile(f):
+	data = NifFormat.Data()
+
+	print('reading ' + f + ' ...', flush=True)
+	with open(f, 'rb') as stream:
+		data.read(stream)
+#	print(' done!')
+
+	niTriShapes = []
+	for b in data.roots[0].get_extra_datas():
+		if type(b) is NifFormat.NiStringExtraData:
+			if b.name.decode('utf-8') == 'HDT Skinned Mesh Physics Object':
+				data.roots[0].remove_extra_data(b)
+				break
+	for c in data.roots[0].children:
+		if type(c) is NifFormat.NiTriShape:
+			niTriShapes.append(c)
+			match = rec.search(c.name.decode('utf-8'))
+			if match:
+				raise Exception('Fatal error: niTriShape \"' + c.name.decode('utf-8') + '\" has illegal character: \'' + match.group() + '\'')
+
+	if len(niTriShapes) <= 0:
+		print('WARNING: ' + f + ': no niTriShapes found!', file=sys.stderr)
+
+
+	#create Dir
+	nifDir = f + extraParts
+	os.makedirs(nifDir, exist_ok=True)
+
+	#remove_child(child)
+	for shape in niTriShapes:
+		data.roots[0].remove_child(shape)
+
+	#add_child(child)
+	for i,shape in enumerate(niTriShapes):
+		data.roots[0].add_child(shape)
+		shapeFile = nifDir + '/' + str(i).zfill(3) + '_' + shape.name.decode('utf-8').replace(':', ';') + '.nif'
+#		print('writing ' + shapeFile + ' ...', end='', flush=True)
+		with open(shapeFile, 'wb') as shapeStream:
+			data.write(shapeStream)
+		data.roots[0].remove_child(shape)
+#		print(' done!')
+
+if __name__ == '__main__':
+	mp.freeze_support()
+	#lower the prio and obey process affinity mask
+	try:
+		sys.getwindowsversion()
+	except AttributeError: #Unix
+		os.nice(19)
+		poolsize = len(os.sched_getaffinity(0))
+	else: #Windows
+		os.system("wmic process where processid=\""+str(os.getpid())+"\" CALL   setpriority \"idle\"")
+
+		#thanks to eryksun: https://bugs.python.org/msg236867
+		from ctypes import *
+		from ctypes.wintypes import *
+		
+		kernel32 = WinDLL('kernel32')
+		
+		DWORD_PTR = WPARAM
+		PDWORD_PTR = POINTER(DWORD_PTR)
+		
+		GetCurrentProcess = kernel32.GetCurrentProcess
+		GetCurrentProcess.restype = HANDLE
+		
+		OpenProcess = kernel32.OpenProcess
+		OpenProcess.restype = HANDLE
+		OpenProcess.argtypes = (DWORD, # dwDesiredAccess,_In_
+		                        BOOL,  # bInheritHandle,_In_
+		                        DWORD) # dwProcessId, _In_
+		
+		GetProcessAffinityMask = kernel32.GetProcessAffinityMask
+		GetProcessAffinityMask.argtypes = (
+		    HANDLE,     # hProcess, _In_
+		    PDWORD_PTR, # lpProcessAffinityMask, _Out_
+		    PDWORD_PTR) # lpSystemAffinityMask, _Out_
+		
+		SetProcessAffinityMask = kernel32.SetProcessAffinityMask
+		SetProcessAffinityMask.argtypes = (
+		  HANDLE,    # hProcess, _In_
+		  DWORD_PTR) # dwProcessAffinityMask, _In_
+		
+		PROCESS_SET_INFORMATION = 0x0200
+		PROCESS_QUERY_INFORMATION = 0x0400
+		PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
+		if sys.getwindowsversion().major < 6:
+			PROCESS_QUERY_LIMITED_INFORMATION = PROCESS_QUERY_INFORMATION
+		
+		def sched_getaffinity(pid):
+			if pid == 0:
+				hProcess = GetCurrentProcess()
+			else:
+				hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,
+			                           False, pid)
+			if not hProcess:
+				raise WinError()
+			lpProcessAffinityMask = DWORD_PTR()
+			lpSystemAffinityMask = DWORD_PTR()
+			if not GetProcessAffinityMask(hProcess,
+			                              byref(lpProcessAffinityMask),
+			                              byref(lpSystemAffinityMask)):
+				raise WinError()
+			mask = lpProcessAffinityMask.value
+			return {c for c in range(sizeof(DWORD_PTR) * 8) if (1 << c) & mask}
+		poolsize = len(sched_getaffinity(0))
+
+
+	print('starting ' + str(poolsize) + ' worker threads')
+	p = mp.Pool(poolsize)
+	result = p.map_async(convertFile, files, 1)
+	try:
+		result.get()
+	except Exception as error:
+		print(error.args[0], file=sys.stderr, flush=True)
+	finally:
+		p.terminate()
+		p.join()
+		input('Press Enter to close the program')
---
+++ xEdit script/ARMO_2_HDPT_convert.pas
@@ -0,0 +1,196 @@
+// ***** BEGIN LICENSE BLOCK *****
+//
+//Copyright (c) 2021, yeahhowaboutnooo.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+//
+// ***** END LICENSE BLOCK *****
+
+unit UserScript;
+
+
+function Initialize: integer;
+begin
+  Result := 0;
+end;
+
+
+function Process(eSource: IInterface): integer;
+var
+  armorAddon, eSourceAAs, extraParts: IwbElement;
+  espFile, hdpt_entry, eTarget, extraTarget: IInterface;
+  searchResultName, eTargetName, extraTargetName, nifFileName, extraPartDir, fullExtraPartDir, extraPartName, extraFileName: string;
+  searchResult: TSearchRec;
+  extraTargetCounter, aaCount, extraPartCntr: integer;
+begin
+  Result := 0;
+
+  AddMessage('Processing: ' + Name(eSource));
+
+  espfile := getfile(esource);
+
+  hdpt_entry := Add(espFile, 'HDPT', true);
+
+  eTarget := Add(hdpt_entry, 'HDPT', true);
+  if not ProcessArmor(eSource, eTarget, hdpt_entry, 'Male') then Remove(eTarget);
+
+  eTarget := Add(hdpt_entry, 'HDPT', true);
+  if not ProcessArmor(eSource, eTarget, hdpt_entry, 'Female') then Remove(eTarget);
+end;
+
+function ProcessArmor(eSource, eTarget, hdpt_entry: IInterface; gender: string): boolean;
+var
+  armorAddon, eSourceAAs, extraParts: IwbElement;
+  extraTarget: IInterface;
+  genderModelString, searchResultName, eTargetName, extraTargetName, nifFileName, extraPartDir, fullExtraPartDir, extraPartName, extraFileName: string;
+  searchResult: TSearchRec;
+  extraTargetCounter, aaCount, extraPartCntr: integer;
+  isGenderCompatible: boolean;
+begin
+  if gender = 'Female' then
+  begin
+    genderModelString := 'Female world model\MOD3 - Model FileName';
+    eTargetName := 'HDPT_F_' + EditorID(eSource);
+    isGenderCompatible := true;
+  end;
+  if gender = 'Male' then
+  begin
+    genderModelString := 'Male world model\MOD2 - Model FileName';
+    eTargetName := 'HDPT_M_' + EditorID(eSource);
+    isGenderCompatible := false;
+  end;
+
+  eSourceAAs := ElementByPath(eSource, 'Armature');
+  //run through all armor addons twice:
+  //first pass: check if we have all required meshes to create a headpart for the requested gender
+  for aaCount := 0 to ElementCount(eSourceAAs) -1 do
+  begin
+    armorAddon := LinksTo(ElementByIndex(eSourceAAs, aaCount));
+
+    //female headparts have a collision-body
+    // -> a female armor has female world models for all of its corresponding armor addons
+    //whereas a male armor only needs one male world model for any of its corresponding armor addons
+    if gender = 'Female' then
+    begin
+      isGenderCompatible := isGenderCompatible and ElementExists(armorAddon, genderModelString);
+    end;
+    if gender = 'Male' then
+    begin
+      isGenderCompatible := isGenderCompatible or ElementExists(armorAddon, genderModelString);
+    end;
+  end;
+  Result := False;
+  if not isGenderCompatible then Exit;
+  if (ElementCount(eSourceAAs) <= 0) then Exit;
+
+
+  BeginUpdate(eTarget);
+    SetEditorID(eTarget, eTargetName);
+
+    Add(eTarget, 'NAME', true);
+    SetElementEditValues(eTarget, 'FULL - Name', eTargetName);
+
+    Add(eTarget, 'RNAM', true);
+    SetElementEditValues(eTarget, 'RNAM - Valid Races', 'HeadPartsAllRacesMinusBeast [FLST:000A803F]');
+
+    Add(eTarget, 'PNAM', true);
+    SetElementEditValues(eTarget, 'PNAM - Type', 'Hair');
+
+    //bit0: Playable, bit1: Male and bit2: Female
+    if gender = 'Female' then SetElementEditValues(eTarget, 'DATA - Flags', '101');
+    if gender = 'Male'   then SetElementEditValues(eTarget, 'DATA - Flags', '110');
+
+    extraParts := ElementByName(eTarget, 'Extra Parts');
+    RemoveElement(eTarget, extraParts);
+    extraPartCntr := 0;
+
+
+    //2nd pass through all armor addons: if we even got to this point -> we have all required meshes for the requested gender
+    for aaCount := 0 to ElementCount(eSourceAAs) -1 do
+    begin
+      armorAddon := LinksTo(ElementByIndex(eSourceAAs, aaCount));
+      if not ElementExists(armorAddon, genderModelString) then continue;
+
+      //we need the nifFileName to get the extraPartDir
+      //and first extrapart is the nifFile itself (so e.g. the bodyslide-built collision body is always being used)
+      nifFileName := GetElementEditValues(armorAddon, genderModelString);
+      extraPartDir := nifFileName + '__HDPT_extraParts';
+      fullExtraPartDir := DataPath + 'meshes\' + extraPartDir;
+
+      if not directoryexists(fullExtraPartDir) then
+        raise Exception.Create('Fatal error: Could not find extra Parts directory ' + extraPartDir + '!');
+      if FindFirst(fullExtraPartDir + '\*.nif', faAnyFile, searchResult) = 0 then
+      begin
+        extraTargetCounter := 0;
+        repeat
+          if searchResult.Attr <> faDirectory then
+          begin
+            searchResultName := searchResult.name;
+            Delete(searchResultName, 1, 4); //get rid of the index at the beginning of the file
+            extraTargetCounter := extraTargetCounter + 1;
+            extraTargetName := eTargetName + '_extraPart' + inttostr(extraPartCntr);
+            extraTarget := Add(hdpt_entry, 'HDPT', true);
+            BeginUpdate(extraTarget);
+              //get rid of the file extension and replace ; with :
+              //(as windows filenames are not allowed to contain : but .nif-meshnames sometimes contain : )
+              SetEditorID(extraTarget, StringReplace(ChangeFileExt(searchResultName, ''), ';', ':', [rfReplaceAll]));
+              Add(extraTarget, 'NAME', true);
+              SetElementEditValues(extraTarget, 'FULL - Name', extraTargetName);
+
+              Add(extraTarget, 'MODL', true);
+              SetElementEditValues(extraTarget, 'Model\MODL - Model FileName', extraPartDir + '\' + searchResult.name);
+              if extraTargetCounter = 1 then
+                SetElementEditValues(extraTarget, 'Model\MODL - Model FileName', nifFileName);
+
+              Add(extraTarget, 'RNAM', true);
+              //HeadPartsAllRacesMinusBeast [FLST:000A803F]
+              SetElementEditValues(extraTarget, 'RNAM - Valid Races', 'HeadPartsAllRacesMinusBeast [FLST:000A803F]');
+
+              Add(extraTarget, 'PNAM', true);
+              SetElementEditValues(extraTarget, 'PNAM - Type', 'Misc');
+
+              //bit0: Playable, bit1: Male, bit2: Female and bit3: isExtraPart
+              if gender = 'Female' then SetElementEditValues(extraTarget, 'DATA - Flags', '1011');
+              if gender = 'Male'   then SetElementEditValues(extraTarget, 'DATA - Flags', '1101');
+            EndUpdate(extraTarget);
+
+            extraParts := ElementByName(eTarget, 'Extra Parts');
+            if not Assigned(extraParts) then
+              SetEditValue( ElementByIndex(Add(eTarget, 'Extra Parts', True), 0), Name(extraTarget))
+            else
+              SetEditValue( ElementAssign(extraParts, HighInteger, nil, False), Name(extraTarget));
+            extraPartCntr := extraPartCntr + 1;
+          end;
+        until FindNext(searchResult) <> 0;
+        FindClose(searchResult);
+      end;
+    end;
+  EndUpdate(eTarget);
+  Result := True;
+  Exit;
+end;
+
+
+function Finalize: integer;
+begin
+    Result := 1;
+    exit;
+end;
+
+end.
